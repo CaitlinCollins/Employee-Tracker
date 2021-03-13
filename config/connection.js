2440b8ae122db0aa16a436f1833d7aec4c24a201
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-12 20:03:24 -0600

    Ran npm init and built connection.js

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require ("mysql");
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: process.env.DB_USER,
+  
+    // Your password
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+  });
+
+module.exports = connection;
